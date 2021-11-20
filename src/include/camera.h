d56487ac4424bf85c557ae6e94ef4918ccb36a57
ChoKiyeon <qsky8520@gmail.com>
2021-11-20 10:12:17 +0900

    camera 추가

---
+++ src/include/camera.h
@@ -0,0 +1,19 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "gfx.h"
+#include "util.h"
+
+// GLFW의 카메라
+
+struct Camera
+{
+    mat4s view, proj;
+    vec3s position, direction, up, right;
+    f32 pitch, yaw, fov, aspect, znear, zfar;
+};
+
+struct Camera camera_create(f32 fov);
+void camera_update(struct Camera *self);
+
+#endif
